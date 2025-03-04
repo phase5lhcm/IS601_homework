268053a086d4111fbe2f3758b563de74692a64ed
cmaynard <christine.ri.maynard@gmail.com>
2025-03-04 18:37:55 -0500

    Update imports

--- calculator/commands/subtract.py
+++ calculator/commands/subtract.py
@@ -1,8 +1,8 @@
 #Inheirits from the Command base class to implement the subtraction operation via the execute method
 from .command import Command
-from utils.validator import Validator
+from ..utils.validator import Validator
 
-class Add(Command):
+class Subtract(Command):
     def __init__(self, first_operand, second_operand):
         self.first_operand = first_operand
         self.second_operand = second_operand
--- tests/command_tests.py
+++ tests/command_tests.py
@@ -1,9 +1,9 @@
 import pytest
 from faker import Faker
-from commands.add import Add
-from commands.subtract import Subtract
-from commands.multiply import Multiply
-from commands.divide import Divide
+from calculator.commands.add import Add
+from calculator.commands.subtract import Subtract
+from calculator.commands.multiply import Multiply
+from calculator.commands.divide import Divide
 
 fake = Faker()
 
@@ -25,9 +25,16 @@ def test_multiply_command():
     command = Multiply(first_operand, second_operand)
     assert command.execute() == first_operand * second_operand
 
+#test division by non-zero numbers
 def test_divide_command():
     first_operand = fake.random_int(min=1, max=100)
     second_operand = fake.random_int(min=1, max=10)
     command = Divide(first_operand, second_operand)
     assert command.execute() == first_operand / second_operand
 
+#test division by zero
+def test_divide_by_zero():
+    operand1 = fake.random_int(min=1, max=100)
+    command = Divide(operand1, 0)
+    with pytest.raises(ValueError):
+        command.execute()

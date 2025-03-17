4332c1eef6c4110666c90375afef36dd295b748a
cmaynard <christine.ri.maynard@gmail.com>
2025-03-16 21:57:55 -0400

    Attempt at getting GitHub actions to run tests

--- calculator/commands/divide.py
+++ calculator/commands/divide.py
@@ -4,10 +4,10 @@ from ..utils.validator import Validator
 
 
 class Divide(Command):
-    def __init__(self, first_operand, second_operand):
-        self.first_operand = first_operand
-        self.second_operand = second_operand
+    def __init__(self, a,b):
+        self.a = a
+        self.b = b
     
     def execute(self):
-        Validator.validate(self.second_operand,"Cannot divide by zero" )
-        return self.first_operand / self.second_operand
+        Validator.validate(self.b,"Cannot divide by zero" )
+        return self.a / self.b
